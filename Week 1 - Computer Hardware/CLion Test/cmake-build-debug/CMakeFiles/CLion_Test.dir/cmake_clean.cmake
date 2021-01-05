14388646dc8d3a75fca65ffed92e9789ca835650
saraaahh63 <sat454@nyu.edu>
2021-01-05 15:14:31 +0300

    CLion Test

---
+++ Week 1 - Computer Hardware/CLion Test/cmake-build-debug/CMakeFiles/CLion_Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLion_Test"
+  "CLion_Test.pdb"
+  "CMakeFiles/CLion_Test.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
